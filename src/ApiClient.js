f6e35660cbbc67e41a166ab62aa7f4bb59d4b0ea
Elliot <130664947+ellioht@users.noreply.github.com>
2023-07-14 23:46:27 +0100

    tailwind

--- src/ApiClient.js
+++ src/ApiClient.js
@@ -1,47 +1,11 @@
 import axios from "axios";
 
-const apiKey = process.env.REACT_APP_API_KEY;
-const multipleUrl = "https://random-words5.p.rapidapi.com/getMultipleRandom";
-const singleUrl = "https://random-words5.p.rapidapi.com/getRandom";
+// const apiKey = process.env.REACT_APP_API_KEY;
+const API_URL = "https://random-word-api.vercel.app";
 
-export class ApiClient {
-  getWords() {
-    return axios
-      .get(multipleUrl, {
-        headers: {
-          "x-rapidapi-key": apiKey,
-          "x-rapidapi-host": "random-words5.p.rapidapi.com",
-        },
-        params: {
-          count: 8,
-          minLength: 5,
-          maxLength: 10,
-        },
-      })
-      .catch((error) => {
-        if (error.response) {
-          console.log(error.response.headers);
-        }
-        throw error;
-      });
-  }
-  getWord() {
-    return axios
-      .get(singleUrl, {
-        headers: {
-          "x-rapidapi-key": apiKey,
-          "x-rapidapi-host": "random-words5.p.rapidapi.com",
-        },
-        params: {
-          minLength: 5,
-          maxLength: 10,
-        },
-      })
-      .catch((error) => {
-        if (error.response) {
-          console.log(error.response.headers);
-        }
-        throw error;
-      });
-  }
+export default class ApiClient {
+  static getWords(words) {
+    const url = `${API_URL}/api?words=${words}`;
+    return axios.get(url);
+  };
 }
